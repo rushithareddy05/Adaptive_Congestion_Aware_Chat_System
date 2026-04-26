3ee04c8dc21f523a519eb24f508fbe7ed606f49e
Rushitha Reddy <grushithareddy9@gmail.com>
2026-04-26 12:40:18 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://adaptive-congestion-aware-chat-system.onrender.com", {
-  transports: ["websocket"]
-});
+export const socket = io(
+  "https://adaptive-congestion-aware-chat-system.onrender.com",
+  {
+    transports: ["websocket", "polling"]
+  }
+);
